c5264d5d7ae41ca4eccbe89cfb0f9089a9d29351
Милош Поповић <gpopac@gmail.com>
2018-12-23 21:51:46 +0100

    Move taxon fetching to a separate class.

--- app/src/main/java/org/biologer/biologer/FetchTaxa.java
+++ app/src/main/java/org/biologer/biologer/FetchTaxa.java
@@ -1,8 +1,101 @@
 package org.biologer.biologer;
 
-public abstract class FetchTaxa {
-    /*
-    * This is a work in progress, since I intend to move taxon fetching into a separate class! :)
-    */
+import android.app.Activity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import org.biologer.biologer.model.Stage;
+import org.biologer.biologer.model.network.Stage6;
+import org.biologer.biologer.model.network.TaksoniResponse;
+import org.biologer.biologer.model.network.Taxa;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Response;
+
+public abstract class FetchTaxa extends Activity {
+
+    private static int totalPages = 1;
+    private static int progressStatus = 0;
+
+    public static void fetchAll(final int page) {
+        if (page > totalPages) {
+            //progressBar.setVisibility(View.GONE);
+            return;
+        }
+
+        Call<TaksoniResponse> call = App.get().getService().getTaxons(page, 50);
+
+        call.enqueue(new CallbackWithRetry<TaksoniResponse>(call) {
+            @Override
+            public void onResponse(Call<TaksoniResponse> call, Response<TaksoniResponse> response) {
+                if (1 == page) {
+                    App.get().getDaoSession().getStageDao().deleteAll();
+                    totalPages = response.body().getMeta().getLastPage();
+                }
+
+                List<Taxa> taxa = response.body().getData();
+
+                int lastPage = response.body().getMeta().getLastPage();
+                int currentPage = response.body().getMeta().getCurrentPage();
+
+                // Update the Progress Bar status
+                progressStatus = (page * 100 / totalPages);
+                getProgressStatus();
+                //progressBar.setProgress(progressStatus);
+
+                // Print the response from the server in logcat for debugging.
+                // Log.w("Izlaz za logcat",new Gson().toJson(response));
+                // Log.i("Taxon list > ",new GsonBuilder().setPrettyPrinting().create().toJson(response.body()));
+                // Log.i("This page is No. > ", Integer.valueOf(page).toString());
+                // Log.i("Last page is No.  > ", Integer.valueOf(totalPages).toString());
+
+                for (Taxa taxon : taxa) {
+                    App.get().getDaoSession().getTaxonDao().insertOrReplace(taxon.toTaxon());
+
+                    List<Stage6> stages = taxon.getStages();
+
+                    for (Stage6 stage : stages) {
+                        App.get().getDaoSession().getStageDao().insert(new Stage(null, stage.getName(), stage.getId(), taxon.getId()));
+                    }
+                }
+
+                // If we just finished fetching taxa data for the last page, we can stop showing
+                // loader. Otherwise we continue fetching taxa from the API on the next page.
+                if (isLastPage(page)) {
+                    // Hide the Progress bar
+                    //progressBar.setVisibility(View.GONE);
+                    // Inform the user of success
+                    //Toast.makeText(getActivity(), getString(R.string.database_updated), Toast.LENGTH_LONG).show();
+                    Log.i("Fetching taxa > ", "All taxa were successfully updated from the server!");
+                } else {
+                    fetchAll(page + 1);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<TaksoniResponse> call, Throwable t) {
+                // Remove partially retrieved data from the database
+                App.get().getDaoSession().getStageDao().deleteAll();
+                // Hide the Progress bar
+                //progressBar.setVisibility(View.GONE);
+                // Inform the user on failure and write log message
+                //Toast.makeText(getActivity(), getString(R.string.database_connect_error), Toast.LENGTH_LONG).show();
+                Log.e("Fetching taxa > ", "Application could not get data from a server!");
+            }
+        });
+    }
+
+    private static boolean isLastPage(int page) {
+        return page == totalPages;
+    }
+
+    public static int getProgressStatus() {
+        //Log.e("STATUS > ", String.valueOf(progressStatus));
+        return progressStatus;
+    }
 
 }
--- app/src/main/java/org/biologer/biologer/SetupFragment.java
+++ app/src/main/java/org/biologer/biologer/SetupFragment.java
@@ -1,5 +1,6 @@
 package org.biologer.biologer;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -7,24 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
 import android.widget.ProgressBar;
-import android.widget.Toast;
-
-import org.biologer.biologer.model.Stage;
-import org.biologer.biologer.model.network.Stage6;
-import org.biologer.biologer.model.network.TaksoniResponse;
-import org.biologer.biologer.model.network.Taxa;
-
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Response;
 
 public class SetupFragment extends Fragment {
 
-    private int totalPages = 1;
     private ProgressBar progressBar;
-    int progressStatus;
+    private int oldProgress = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -38,83 +28,41 @@ public class SetupFragment extends Fragment {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 progressBar.setVisibility(View.VISIBLE);
-                totalPages = 1;
-                fetchTaxa(1);
+
+                Thread updateStatusBar = new Thread() {
+
+                    @Override
+                    public void run() {
+                        try {
+                            sleep(1000);
+                            while (progressBar.getProgress() < 100) {
+                                int progress_value = FetchTaxa.getProgressStatus();
+                                if (progress_value != oldProgress) {
+                                    oldProgress = progress_value;
+                                    progressBar.setProgress(progress_value);
+                                }
+                            }
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        } finally {
+                            getActivity().runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    progressBar.setVisibility(View.GONE);
+                                }
+                            });
+                        }
+                    }
+                };
+
+                updateStatusBar.start();
+                FetchTaxa.fetchAll(1);
+
             }
         });
 
         return root;
     }
-
-    public void fetchTaxa(final int page) {
-        if (page > totalPages) {
-            progressBar.setVisibility(View.GONE);
-            return;
-        }
-
-        Call<TaksoniResponse> call = App.get().getService().getTaxons(page, 60);
-
-        call.enqueue(new CallbackWithRetry<TaksoniResponse>(call) {
-            @Override
-            public void onResponse(Call<TaksoniResponse> call, Response<TaksoniResponse> response) {
-                if (1 == page) {
-                    App.get().getDaoSession().getStageDao().deleteAll();
-                    totalPages = response.body().getMeta().getLastPage();
-                }
-
-                List<Taxa> taxa = response.body().getData();
-
-                int lastPage = response.body().getMeta().getLastPage();
-                int currentPage = response.body().getMeta().getCurrentPage();
-
-                // Update the Progress Bar status
-                progressStatus = (page *100 / totalPages);
-                progressBar.setProgress(progressStatus);
-
-                // Print the response from the server in logcat for debugging.
-                // Log.w("Izlaz za logcat",new Gson().toJson(response));
-                // Log.i("Taxon list > ",new GsonBuilder().setPrettyPrinting().create().toJson(response.body()));
-                // Log.i("This page is No. > ", Integer.valueOf(page).toString());
-                // Log.i("Last page is No.  > ", Integer.valueOf(totalPages).toString());
-
-                for (Taxa taxon : taxa) {
-                    App.get().getDaoSession().getTaxonDao().insertOrReplace(taxon.toTaxon());
-
-                    List<Stage6> stages = taxon.getStages();
-
-                    for (Stage6 stage : stages) {
-                        App.get().getDaoSession().getStageDao().insert(new Stage(null, stage.getName(), stage.getId(), taxon.getId()));
-                    }
-                }
-
-                // If we just finished fetching taxa data for the last page, we can stop showing
-                // loader. Otherwise we continue fetching taxa from the API on the next page.
-                if (isLastPage(page)) {
-                    // Hide the Progress bar
-                    progressBar.setVisibility(View.GONE);
-                    // Inform the user of success
-                    Toast.makeText(getActivity(), getString(R.string.database_updated), Toast.LENGTH_LONG).show();
-                } else {
-                    fetchTaxa(page + 1);
-                }
-            }
-
-            @Override
-            public void onFailure(Call<TaksoniResponse> call, Throwable t) {
-                // Remove partially retrieved data from the database
-                App.get().getDaoSession().getStageDao().deleteAll();
-                // Hide the Progress bar
-                progressBar.setVisibility(View.GONE);
-                // Inform the user on failure and write log message
-                Toast.makeText(getActivity(), getString(R.string.database_connect_error), Toast.LENGTH_LONG).show();
-                Log.e("Fetching taxa failed > ", "Application could not get data from a server!");
-            }
-        });
-    }
-
-    private boolean isLastPage(int page) {
-        return page == this.totalPages;
-    }
-
 }
